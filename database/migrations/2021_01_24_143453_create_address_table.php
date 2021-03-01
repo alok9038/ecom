b21bb49bfb7c6f8fec9536bfd88c3305370053fd
alok9038 <kumaralok1884@gmail.com>
2021-03-01 13:35:10 +0530

    https server

--- database/migrations/2021_01_24_143453_create_address_table.php
+++ database/migrations/2021_01_24_143453_create_address_table.php
@@ -13,7 +13,7 @@ class CreateAddressTable extends Migration
      */
     public function up()
     {
-        Schema::create('address', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('contact');
